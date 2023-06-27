0691c6ce8feccf889cdc7e2437d5d7f5a124e98a
Chattipoom <chattipoom.siri@gmail.com>
2023-06-27 10:28:50 +0700

    Clean up

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,15 +8,12 @@ import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 function HomepageHeader() {
   const { siteConfig } = useDocusaurusContext();
   return (
-    <header className={clsx("hero hero--primary", styles.heroBanner)}>
+    <header className={clsx("hero", styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/docs/intro"
-          >
+          <Link className="button button--primary button--lg" to="/docs/intro">
             อ่านคู่มือ
           </Link>
         </div>
