5d628d7e0f0612b1c76b097ce9c49a2dfed9edc1
Chattipoom <chattipoom.siri@gmail.com>
2023-06-27 11:16:56 +0700

    Update config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://KireinaSoft.github.io/kku-drone-user-docs",
+  url: "https://KireinaSoft.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/",
+  baseUrl: "/kku-drone-user-docs",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
